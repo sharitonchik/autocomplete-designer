3d43a88acdb13f2d4bf36e105e209a2a4a069cc3
Sergey Haritonchik <berreig@gmail.com>
2012-03-20 11:53:39 +0300

    new implementation

--- src/main/java/by/mgvrk/entity/User.java
+++ src/main/java/by/mgvrk/entity/User.java
@@ -12,22 +12,63 @@ public class User {
     private String country;
     private String role;
 
-    public User(int ID, String name, String password) {
-        this.ID = ID;
-        this.name = name;
-        this.password = password;
+    public User() {
     }
 
     public int getID() {
         return ID;
     }
 
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
 
